9781d0366a3830cc8142315cedcec55b39d922e9
tom pellecchia <tom.pellecchia1@gmail.com>
2024-11-21 14:57:31 +0100

    chessboard pieces initial position corrected

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,34 +2,18 @@
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 
+import ChessBoard from './components/ChessBoard.vue'
+
 import { socket } from "@/socket";
 
 socket.emit("message", "test");
 
-
-
-
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <ChessBoard></ChessBoard>
 </template>
+
 
 <style scoped>
 header {
