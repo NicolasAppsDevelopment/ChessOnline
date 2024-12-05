3ea999ab41284d067bd65e7f3610e497e1f90d8c
tom pellecchia <tom.pellecchia1@gmail.com>
2024-12-05 14:32:51 +0100

    display chessboard and pieces (not finished but work)

--- client/src/components/ChessBoard.vue
+++ client/src/components/ChessBoard.vue
@@ -1,21 +1,54 @@
 <template>
- 
+
+  <!--  TODO Rotate the print of the chessboard -->
   <table class="table">
     <tbody>
         <tr v-for="column in chessBoard?.board">
-            <td v-for="cell in column" >{{[cell]}}</td>
+            <td v-for="cell in column" class="square">
+              <img v-if="cell?.getSprite()" v-bind:src="cell?.getSprite()" v-bind:alt="cell?.getColor() + ' ' + cell?.getName()" />
+              <div v-if="!cell?.getSprite()"> </div>
+            </td>
         </tr>
     </tbody>
+    <tfoot>
+      <tr>
+        <td><span>8</span></td>
+        <td><span>7</span></td>
+        <td><span>6</span></td>
+        <td><span>5</span></td>
+        <td><span>4</span></td>
+        <td><span>3</span></td>
+        <td><span>2</span></td>
+        <td><span>1</span></td>
+      </tr>
+    </tfoot>
   </table>
-  
+
 </template>
 
 <script setup lang="ts">
 import type { Chessboard } from '@/class/chessboard';
 const chessBoard = defineModel<Chessboard>('chessBoard', {});
 
-console.log(chessBoard.value?.board[0])
+/**
+document.addEventListener("DOMContentLoaded", (event) => {
+  const squares = document.getElementsByClassName("square");
+  console.log(squares);
+  for (let row=0;row<8;row++) {
+    for (let column=0;column<8;column++) {
+      if ((row + column) % 2) {
+        squares[(row + column)].className = 'black';
+      } else {
+        squares[(row + column)].className = 'white';
+      }
+    }
+  }
+});
+*/
+
+
 </script>
 
 <style scoped>
+@import '@/assets/main.css';
 </style>
--- server/src/class/chessboard.ts
+++ server/src/class/chessboard.ts
@@ -11,48 +11,48 @@ export class Chessboard {
 
   constructor() {
     this.board = [];
-    for(var i: number = 0; i < 7; i++) {
+    for(let i: number = 0; i <= 7; i++) {
       this.board[i] = [];
-      for(var j: number = 0; j< 7; j++) {
+      for(let j: number = 0; j <= 7; j++) {
         this.board[i][j] = null;
       }
     }
 
-    //Create and place the blacks pieces in the chessboard
-    this.board[0][7] = new Rook(Color.Black,0,7);
-    this.board[7][7] = new Rook(Color.Black,7,7);
-    this.board[1][7] = new Knight(Color.Black,1,7);
-    this.board[6][7] = new Knight(Color.Black,6,7);
-    this.board[2][7] = new Bishop(Color.Black,2,7);
-    this.board[5][7] = new Bishop(Color.Black,5,7);
-    this.board[3][7] = new Queen(Color.Black,3,7);
-    this.board[4][7] = new King(Color.Black,4,7);
-    this.board[0][6] = new Pawn(Color.Black,0,6);
-    this.board[1][6] = new Pawn(Color.Black,1,6);
-    this.board[2][6] = new Pawn(Color.Black,2,6);
-    this.board[3][6] = new Pawn(Color.Black,3,6);
-    this.board[4][6] = new Pawn(Color.Black,4,6);
-    this.board[5][6] = new Pawn(Color.Black,5,6);
-    this.board[6][6] = new Pawn(Color.Black,6,6);
-    this.board[7][6] = new Pawn(Color.Black,7,6);
-
     //Create and place the whites pieces in the chessboard
-    this.board[0][0] = new Rook(Color.White,0,0);
-    this.board[7][0] = new Rook(Color.White,7,0);
-    this.board[1][0] = new Knight(Color.White,1,0);
-    this.board[6][0] = new Knight(Color.White,6,0);
-    this.board[2][0] = new Bishop(Color.White,2,0);
-    this.board[5][0] = new Bishop(Color.White,5,0);
-    this.board[3][0] = new Queen(Color.White,3,0);
-    this.board[4][0] = new King(Color.White,4,0);
-    this.board[0][1] = new Pawn(Color.White,0,1);
-    this.board[1][1] = new Pawn(Color.White,1,1);
-    this.board[2][1] = new Pawn(Color.White,2,1);
-    this.board[3][1] = new Pawn(Color.White,3,1);
-    this.board[4][1] = new Pawn(Color.White,4,1);
-    this.board[5][1] = new Pawn(Color.White,5,1);
-    this.board[6][1] = new Pawn(Color.White,6,1);
-    this.board[7][1] = new Pawn(Color.White,7,1);
+    this.board[0][7] = new Rook(Color.White,0,7);
+    this.board[7][7] = new Rook(Color.White,7,7);
+    this.board[1][7] = new Knight(Color.White,1,7);
+    this.board[6][7] = new Knight(Color.White,6,7);
+    this.board[2][7] = new Bishop(Color.White,2,7);
+    this.board[5][7] = new Bishop(Color.White,5,7);
+    this.board[3][7] = new Queen(Color.White,3,7);
+    this.board[4][7] = new King(Color.White,4,7);
+    this.board[0][6] = new Pawn(Color.White,0,6);
+    this.board[1][6] = new Pawn(Color.White,1,6);
+    this.board[2][6] = new Pawn(Color.White,2,6);
+    this.board[3][6] = new Pawn(Color.White,3,6);
+    this.board[4][6] = new Pawn(Color.White,4,6);
+    this.board[5][6] = new Pawn(Color.White,5,6);
+    this.board[6][6] = new Pawn(Color.White,6,6);
+    this.board[7][6] = new Pawn(Color.White,7,6);
+
+    //Create and place the blacks pieces in the chessboard
+    this.board[0][0] = new Rook(Color.Black,0,0);
+    this.board[7][0] = new Rook(Color.Black,7,0);
+    this.board[1][0] = new Knight(Color.Black,1,0);
+    this.board[6][0] = new Knight(Color.Black,6,0);
+    this.board[2][0] = new Bishop(Color.Black,2,0);
+    this.board[5][0] = new Bishop(Color.Black,5,0);
+    this.board[3][0] = new Queen(Color.Black,3,0);
+    this.board[4][0] = new King(Color.Black,4,0);
+    this.board[0][1] = new Pawn(Color.Black,0,1);
+    this.board[1][1] = new Pawn(Color.Black,1,1);
+    this.board[2][1] = new Pawn(Color.Black,2,1);
+    this.board[3][1] = new Pawn(Color.Black,3,1);
+    this.board[4][1] = new Pawn(Color.Black,4,1);
+    this.board[5][1] = new Pawn(Color.Black,5,1);
+    this.board[6][1] = new Pawn(Color.Black,6,1);
+    this.board[7][1] = new Pawn(Color.Black,7,1);
 
   }
 }
