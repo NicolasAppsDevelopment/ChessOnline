2e817a9356cf8b4dc784bf7743069e4bb799030e
Nicolas Colomb <ncolomb@myluckyday.fr>
2024-12-28 17:52:07 +0100

    give chess notation id to cells

--- server/src/models/Cell.ts
+++ server/src/models/Cell.ts
@@ -4,11 +4,13 @@ import type {Piece} from "./Piece";
 export class Cell {
   public piece: Piece | null;
   public readonly position: Position;
+  public readonly id: string;
   public isHighlighted: boolean;
 
   constructor(position: Position, piece: Piece | null, isHighlighted: boolean = false) {
     this.position = position;
     this.piece = piece;
     this.isHighlighted = isHighlighted;
+    this.id = String.fromCharCode(65 + this.position.x) + "" + (8 - this.position.y);
   }
 }
